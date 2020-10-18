da07d2aa9b71bf615db6103d9989144603742dda
shunjid <shunjid.rahman@gmail.com>
2020-10-19 00:17:42 +0600

    removed jQuery

--- assets/scripts/materialize-activator.js
+++ assets/scripts/materialize-activator.js
@@ -1,6 +1,11 @@
-$(function() {
-    $('.sidenav').sidenav();
-    $('.tooltipped').tooltip();
-    $('.fixed-action-btn').floatingActionButton();
-    $('.modal').modal();
+document.addEventListener('DOMContentLoaded', function() {
+    const elements = {
+        tooltip: document.querySelectorAll('.tooltipped'),
+        sideNav: document.querySelectorAll('.sidenav'),
+        fixedActionButton: document.querySelector('.fixed-action-btn')
+    }
+
+    M.Tooltip.init(elements.tooltip);
+    M.Sidenav.getInstance(elements.sideNav);
+    M.FloatingActionButton.init(elements.fixedActionButton);
 });
