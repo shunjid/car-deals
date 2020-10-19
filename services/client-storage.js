da9a678427c9d49f6093db1987ca1ca615c7ae65
shunjid <shunjid.rahman@gmail.com>
2020-10-19 19:59:26 +0600

    fixed cache getting issues

--- services/client-storage.js
+++ services/client-storage.js
@@ -28,24 +28,23 @@ const addCars = function (cars, carInstance) {
 const getCars = function (carInstance, lastItemId) {
     const limit = 3;
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         carInstance.keys().then((allKeys) => {
             let startAt = 0;
             let endAt = allKeys.length;
 
-            if (lastItemId.value === null) startAt = 0;
-            else {
-                console.log(allKeys);
-                console.log(`finding: ${lastItemId.value}`);
-                startAt = allKeys.indexOf(lastItemId.value.toString());
-            }
 
-            getLocalForageData(carInstance).then(function (values) {
-                let result = values.reverse();
-                console.log(`Start: ${startAt} End: ${endAt}`);
+            getLocalForageData(carInstance).then(function (allCars) {
+                if (lastItemId.value === null) startAt = 0;
+                else {
+                    const newlyFetchedFirstItem = allCars[allCars.length - limit];
+                    startAt = allCars.findIndex(x => x.id == newlyFetchedFirstItem.id);
+                }
+
+
+                let result = allCars.reverse();
                 result = result.slice(startAt, endAt);
-                console.log(result);
-                lastItemId.value = values[values.length - 1].id;
+                lastItemId.value = allCars[allCars.length - 1].id;
                 resolve(result);
             });
         });
